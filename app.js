af8feddb551595be957d41db962c65bff3a8545a
Gus <guscdev@outlook.com>
2023-06-14 15:40:03 -0600

    Initialize repository for backend

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const rateDate = document.querySelector('#rate_date')
-rateDate.valueAsDate = new Date()
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static('styles'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/index.html'))
+})
+app.listen(3000 ,()=>{
+    console.log('App listening on port 3000')
+})
