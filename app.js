7f39e58c86576da7b2a9fa16626cf7371da9c7c9
Gus <guscdev@outlook.com>
2023-06-14 16:36:25 -0600

    Fist workig MVP

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const fs = require('fs');
+
 
 app.use(express.static('styles'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/index.html'))
 })
+
+app.post('/submitRate', async (req,res)=>{
+    const rate = req.body.rate1
+
+    const textContent = `Hola tengo un valor de ${rate}`
+
+    fs.writeFile(path.join(__dirname,'myfile.txt'), textContent, (err) => {
+        if (err) {
+            console.error('An error occurred while creating the file:', err);
+            return;
+        }
+        res.download(path.join(__dirname,'myfile.txt'))
+    });
+    // res.redirect('/')
+})
+
 app.listen(3000 ,()=>{
     console.log('App listening on port 3000')
 })
